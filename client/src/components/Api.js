5f1b284530af384b3a7102060dcac49ebd453553
SAI30570SREERachapudi <saisreegithub@gmail.com>
2026-04-20 03:23:03 +0530

    API Gateway created

--- client/src/components/Api.js
+++ client/src/components/Api.js
@@ -1,6 +1,6 @@
 // API BASE URL (CHANGE THIS AFTER DEPLOYMENT)
 export const BASEURL =
-     "http://[IP_ADDRESS]" || "http://localhost:8080/";
+  "https://hu60qszdhe.execute-api.ap-south-1.amazonaws.com/";
 
 // Generic API caller (UNCHANGED LOGIC)
 export function callApi(reqmethod, url, data, responseHandler) {
